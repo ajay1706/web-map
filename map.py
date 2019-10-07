6e7548adb0898ac294b169802d494caced57270a
ajay1706 <ajay97456@gmail.com>
2019-10-07 20:05:24 +0530

    adding feature groups

--- map.py
+++ map.py
@@ -1,3 +1,11 @@
 import folium
-map = folium.Map(location=[17.441677,78.364666],zoom_start=6)
+map = folium.Map(location=[17.441677,78.364666],zoom_start=6,tiles="OpenStreetMap")
+
+fg = folium.FeatureGroup(name="My Map")
+
+fg.add_child(folium.Marker(location = [17.441677,78.364666],popup="Hostel",icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
 map.save("Map1.html")
+
