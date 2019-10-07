8d6c05a8dfd1827516d09472bd4a4f752fa5c7d4
ajay1706 <ajay97456@gmail.com>
2019-10-07 22:35:18 +0530

    adding txt file of lat's & lon's

--- map.py
+++ map.py
@@ -2,8 +2,8 @@ import folium
 map = folium.Map(location=[17.441677,78.364666],zoom_start=6,tiles="OpenStreetMap")
 
 fg = folium.FeatureGroup(name="My Map")
-
-fg.add_child(folium.Marker(location = [17.441677,78.364666],popup="Hostel",icon=folium.Icon(color='green')))
+for cordinates in [[17.441677,78.364666],[17.651677,78.408666],]:
+    fg.add_child(folium.Marker(location = cordinates,popup="Hostel",icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
