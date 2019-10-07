c08b79d98e407f0495f9a29eabd5ce8d3b8b9b18
ajay1706 <ajay97456@gmail.com>
2019-10-08 00:10:23 +0530

    adding dynamic color points

--- map.py
+++ map.py
@@ -1,16 +1,23 @@
 import folium
 import pandas
-
-
 data = pandas.read_csv("volcanoes.txt")
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
+
+def color_producer(elevation):
+    if elevation<1500:
+         return 'green'
+    elif 1500<= elevation <3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="OpenStreetMap")
 
 fg = folium.FeatureGroup(name="My Map")
-for lt , ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location = [lt,ln],popup="Hostel",icon=folium.Icon(color='green')))
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location = [lt,ln],popup=str(el) + ' m',icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
